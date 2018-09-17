7a939b02bc315a3eed6e26be9424d02b2fab3abb
Louis <ltv210@nyu.edu>
2018-09-17 23:58:48 +0800

    poundpyramid.js

---
+++ Documents/applab/lab2/poundpyramid.js
@@ -0,0 +1,19 @@
+//challenge 1
+
+//super easy way LOL
+console.log(`   #\n  ###\n #####\n#######`)
+
+//a little harder
+function buildPyramid(n) {
+    for(var i = 0; i < n; i++) {
+        var str = ``;
+        for(var j = 1; j < n - i; j++) {
+            str = str + ` `;
+        }
+        for(var k = 1; k <= (2 * i + 1); k++){
+            str = str + `#`;
+        }
+    console.log(str);
+    }
+}
+buildPyramid(4);
